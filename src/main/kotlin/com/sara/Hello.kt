d991cb1ef4655b63d72887a18b18897d2fa41fac
Sara Wu <yichiwu.726@gmail.com>
2020-02-24 11:35:13 +0800

    1st commit

---
+++ src/main/kotlin/com/sara/Hello.kt
@@ -0,0 +1,14 @@
+package com.sara
+
+fun main(args: Array<String>) {
+//    println("Hello Kotlin")
+//    Human().hello()
+    var h = Human()
+    h.hello()
+}
+
+class Human {
+    fun hello() {
+        println("Hello Kotlin")
+    }
+}
