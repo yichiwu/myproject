f0546dc0b8de056852d862e9faff1af19df4638d
Sara Wu <yichiwu.726@gmail.com>
2020-04-19 18:13:07 +0800

    0419_version1

--- src/main/java/com/sara/student/Student.java
+++ src/main/java/com/sara/student/Student.java
@@ -26,10 +26,31 @@ public class Student {
         System.out.print(name + "\t" + english + "\t" + math +
                 "\t" + getAverage());
         if (getAverage() >= 60) {
-            System.out.println("\tPASS");
+            System.out.print("\tPASS");
         } else {
-            System.out.println("\tFAILED");
+            System.out.print("\tFAILED");
         }
+
+        int average = getAverage();
+        char grading = 'F';
+        switch (average/10) {
+            case 10:
+            case 9:
+                grading = 'A';
+                break;
+            case 8:
+                grading = 'B';
+                break;
+            case 7:
+                grading = 'C';
+                break;
+            case 6:
+                grading = 'D';
+                break;
+            default:
+                grading = 'F';
+        }
+        System.out.println("\t" + grading);
     }
 
     public int getAverage() {
--- src/main/kotlin/com/sara/student/Student.kt
+++ src/main/kotlin/com/sara/student/Student.kt
@@ -13,35 +13,30 @@ fun main(args: Array<String>) {
 
 class Student(var name: String?, var english: Int, var math: Int){
     fun print(){
-        println(name + "\t" + english + "\t" + math +
-                "\t" + getAverage() + "\t" +
-                if (getAverage() >= 60) "PASS" else "FAILED"
-        )
+        println("$name\t$english\t$math\t${getAverage()}\t${passOrFailed()}\t${grading()}")
     }
 
-    fun getAverage() : Int {
-        return (english+math)/2
+    fun grading() = when (getAverage()) {
+        in 90..100 -> 'A'
+        in 80..89 -> 'B'
+        in 70..79 -> 'C'
+        in 60..69 -> 'D'
+        else -> 'F'
     }
 
-    fun highest() : Int {
-        var max = if (english > math) {
-            println("english")
-            english
-        } else {
-            println("math")
-            math
-        }
-//        if (english > math) {
-//            max = english
-//        } else {
-//            max = math
-//        }
-        return max
+    fun passOrFailed() = if (getAverage() >= 60) "PASS" else "FAILED"
+
+    fun getAverage() = (english + math) / 2
+
+    fun highest() = if (english > math) {
+        println("english")
+        english
+    } else {
+        println("math")
+        math
     }
 
-    fun nameCheck(){
-        println(name?.length)
-    }
+    fun nameCheck() = name?.length
 }
 
 private fun userInput() {
