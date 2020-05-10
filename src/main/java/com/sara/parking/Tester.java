f8846834b039fca2edb982e465034d58ccd7b301
Sara Wu <yichiwu.726@gmail.com>
2020-05-11 07:16:03 +0800

    0511_version

--- src/main/java/com/sara/parking/Tester.java
+++ src/main/java/com/sara/parking/Tester.java
@@ -1,12 +1,37 @@
 package com.sara.parking;
 
+import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
 import java.util.Date;
 
 public class Tester {
     public static void main(String[] args) {
+//        java8();
+//        java();
+    }
+
+    private static void java8() {
+        Instant instant = Instant.now();
+        System.out.println(instant);
+
+        // Local
+        LocalDateTime now = LocalDateTime.now();
+        System.out.println(now);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        System.out.println(formatter.format(now));
+        System.out.println(now.plus(Duration.ofHours(3)));
+
+        LocalDateTime other = LocalDateTime.of(2020,5, 10, 8, 0, 0);
+        System.out.println(other);
+    }
+
+    private static void java() {
         Date date = new Date();
         System.out.println(date);
         System.out.println(date.getTime());
